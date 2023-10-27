7dd9955f09efdcf2606e9c5b5a4164d09ae2319f
Chloe Chen <xrchen.chloe@gmail.com>
2023-10-26 19:20:14 -0500

    fix

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,6 @@
-import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import Head from 'next/head';
-import Date from '../../components/date';
-import utilStyles from '../../styles/utils.module.css';
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
 
 export default function Post({ postData }) {
   return (
@@ -10,13 +8,6 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
     </Layout>
   );
 }
